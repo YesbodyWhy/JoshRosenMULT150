7f5c13de2c27da9c0d1f16c9302812317871e41c
Your Name <you@example.com>
2024-11-24 23:43:05 -0800

    Hours 14 and 16

---
+++ JRosen_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,7 @@
+/*
+ * The first edit I made, was to the "Size Over Lifetime" Values, by making The size over lifetime start at 0, ramp up to its max value, then slowly shrink down to 0 once more!
+ * Secondly, I changed the "Color Over Lifetime" field to include A purple which fades to a pink before ending on white, and changed the "Rate over Time" to 110.
+ * Thirdly, I changed the "Velocity Over Lifetime" field to make it spiral by changing Orbital Axis to (0,0,-2), Linear Y to 11, and changed the speed modifier to 0.9.
+ * Fourthly, I added the trails, and made their "Width OVer Trail" property get thicker over time with the curve editor!
+ * Lastly, I changed the emission shape to be thicker on the bottom by increasing its angle to 90! YAY MAGIC PORTAL!!
+ */
---
+++ JRosen_Hour14and16/Assets/UserInterface.cs
@@ -0,0 +1,11 @@
+/* Starting off with a basic idea of a "false game" (inspired by the assignment essentially telling us to create the illusion of a game behind a title
+ * screen that doesn't exist) I ran wild! I put a lot more time into this than I probably should have!
+ * Firstly, I imported the Seven - Eleven Font, A VHS Font, and a pixelated font to add variety to the text in this false game! 
+ * I wanted this false game to be enticing and convincing as well, so I ran with the idea and created four (technically six but the fifth one
+ * looks absolutely terrible, and for some reason the sixth one's textures just didn't want to cooperate when imported into Unity, maybe a UV issue?) 
+ * fish models to be used for the title screen! Using the Screen Space - Camera Overlay, I separated the Canvas into two!
+ * One Canvas was named BG for "Background" and included the background elements! Then there was the aptly titled "FrontCanvas" which contained text
+ * and the like. Sandwiched in between the two of these were the Fish Models! Then, on the other side, I overlaid a PSA announcement screen, colored
+ *  monotone red of course, with a Stock image of a man in distress! I also wanted to create a funny but dissonant song, so I whipped this tiny loop
+ *  up in FL! Then, to add a bit of humor, I made it act like pressing the wrong button would give you "liquid blood disorder", which is just having
+ *  normal blood WOMP WOMP! */
