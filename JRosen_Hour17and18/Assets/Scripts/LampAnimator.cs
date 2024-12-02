9eaf9d27a336fa8f866077e120ce412382a0706c
Your Name <you@example.com>
2024-12-01 22:42:50 -0800

    Hour 18

---
+++ JRosen_Hour17and18/Assets/Scripts/LampAnimator.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampAnimator : MonoBehaviour
+{
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            print("W");
+            animator.SetTrigger("Rotate");
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            print("A");
+            animator.SetTrigger("Hover");
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            print("S");
+            animator.SetTrigger("Scale");
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            print("D");
+            animator.SetTrigger("Color");
+        }
+
+    }
+}
